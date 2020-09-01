952213cf649e70ea8bc8e79ea2cb0ccbe377970c
frontendista <csdelreal@gmail.com>
2020-09-01 18:48:51 -0500

    loader to logo animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { motion } from 'framer-motion'
-import { useWindowScrollPosition } from './hooks/useWindowScrollPosition'
+import { motion, AnimateSharedLayout, AnimatePresence } from 'framer-motion'
+// import { useWindowScrollPosition } from './hooks/useWindowScrollPosition'
 
 import './App.css'
 
@@ -10,49 +10,98 @@ import Gallery from './Gallery'
 
 function App() {
 	const [isToggled, setIsToggled] = useState(false)
-	const { y } = useWindowScrollPosition({
-		throttle: 500
-	})
+	const [isLoading, setIsLoading] = useState(true)
+
 	return (
 		<div className="App">
-			<motion.header
-				style={{
-					justifyContent: y > 20 ? 'center' : 'flex-start'
-				}}
-				animate={{
-					background: y > 20 ? 'var(--background)' : 'var(--headerBackground)',
-					color: y > 20 ? '#00214d' : 'white',
-					boxShadow: y > 20 ? 'var(--level-2)' : 'none'
-				}}
-			>
-				<motion.h1 layout className="fake-logo">Level Up</motion.h1>
-			</motion.header>
-			<main className="layout">
-				<button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
-				<Fade isActive={isToggled}>
-					<div className="card">
-						<h4>Post Number one</h4>
-						<p>this is inside the card</p>
-					</div>
-				</Fade>
-				{/* {Simple Slide} */}
-				<Slide isActive={isToggled}>
-					<div className="card">
-						<h4>Post Number one</h4>
-						<p>this is inside the card</p>
-					</div>
-				</Slide>
-				{/* {Complex Slide} */}
-				<Slide direction={-1} distance={600} isActive={isToggled}>
-					<div className="card">
-						<h4>Post Number one</h4>
-						<p>this is inside the card</p>
-					</div>
-				</Slide>
-				<Gallery />
-			</main>
+			<AnimateSharedLayout>
+				<main className="layout">
+					<button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
+					<motion.header layoutId="header" onClick={() => setIsLoading(true)}>
+						<motion.h1 layoutId="logo" className="fake-logo">Logo</motion.h1>
+					</motion.header>
+					<AnimatePresence>
+						{isLoading &&
+							<motion.div
+								layoutId="header"
+								className="loading"
+								onClick={() => setIsLoading(false)}
+							>
+								<motion.h3 layoutId="logo">Title</motion.h3>
+								<motion.div exit={{opacity: 0}}>
+									<p>Is loading...</p>
+									<motion.div
+										initial={{ x: -100 }}
+										animate={{ x: 100 }}
+										transition={{
+											flip: Infinity,
+											duration: 1
+										}}
+										style={{
+											background: 'var(--primary)',
+											height: 6,
+											width: 100,
+										}}
+									/>
+									<motion.div
+										initial={{ x: -180, y: -5 }}
+										animate={{ x: 180, y: -5 }}
+										transition={{
+											flip: Infinity,
+											duration: 1.2
+										}}
+										style={{
+											background: 'var(--danger)',
+											height: 8,
+											width: 20,
+										}}
+									/>
+									<motion.div
+										initial={{ x: -100, y: -5 }}
+										animate={{ x: 100, y: -5 }}
+										transition={{
+											flip: Infinity,
+											duration: 1.5
+										}}
+										style={{
+											background: 'var(--yellow)',
+											height: 4,
+											width: 200,
+										}}
+									/>
+								</motion.div>
+							</motion.div>
+						}
+					</AnimatePresence>
+
+					{/* {Fade Animation} */}
+					<Fade isActive={isToggled}>
+						<div className="card">
+							<h4>Post Number one</h4>
+							<p>this is inside the card</p>
+						</div>
+					</Fade>
+
+					{/* {Simple Slide} */}
+					<Slide isActive={isToggled}>
+						<div className="card">
+							<h4>Post Number one</h4>
+							<p>this is inside the card</p>
+						</div>
+					</Slide>
+
+					{/* {Complex Slide} */}
+					<Slide direction={-1} distance={600} isActive={isToggled}>
+						<div className="card">
+							<h4>Post Number one</h4>
+							<p>this is inside the card</p>
+						</div>
+					</Slide>
+					<Gallery />
+				</main>
+			</AnimateSharedLayout>
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
