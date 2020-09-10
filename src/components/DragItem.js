7c4c403a0bdd729b5d7be01c9a807093c3d229f4
frontendista <csdelreal@gmail.com>
2020-09-09 19:39:04 -0500

    Draggable List with handle

--- src/components/DragItem.js
+++ src/components/DragItem.js
@@ -1,35 +1,41 @@
 import React, { useState } from 'react'
-import { motion } from 'framer-motion'
+import { motion, useMotionValue } from 'framer-motion'
 import { useMeasurePosition } from '../hooks/useMeasurePosition'
 
 function DragItem({ post, updateOrder, updatePosition, i }) {
 	const [isDragging, setIsDragging] = useState(false);
 	const ref = useMeasurePosition((pos) => updatePosition(i, pos))
+	const y = useMotionValue()
 
 	return (
-		<motion.div
-			layout
-			ref={ref}
-			key={post}
-			drag="y"
-			className="card"
-			dragElastic={1}
-			dragConstraints={{ top: 0, bottom: 0 }}
-			onDragStart={() => setIsDragging(true)}
-			onDragEnd={() => setIsDragging(false)}
-			onViewportBoxUpdate={(_, delta) => {
-				if (isDragging) {
-					updateOrder(i, delta.y.translate);
-				}
-			}}
-			animate={{
-				scale: isDragging ? 1.05 : 1,
-				cursor: isDragging ? "grabbing" : "grab"
-			}}
-		>
-			<h4>List Item {post}</h4>
-			<p>this is inside the card</p>
-		</motion.div>
+		<div className="flex">
+			<motion.div style={{ y }} className="card">
+				<h4>List Item {post}</h4>
+				<p>this is inside the card</p>
+			</motion.div>
+			<motion.div
+				layout
+				ref={ref}
+				key={post}
+				drag="y"
+				dragElastic={1}
+				dragConstraints={{ top: 0, bottom: 0 }}
+				onDragStart={() => setIsDragging(true)}
+				onDragEnd={() => setIsDragging(false)}
+				onViewportBoxUpdate={(_, delta) => {
+					if (isDragging) {
+						updateOrder(i, delta.y.translate);
+					}
+					y.set(delta.y.translate)
+				}}
+				animate={{
+					scale: isDragging ? 1.05 : 1,
+					cursor: isDragging ? 'grabbing': 'grab',
+				}}
+			>
+				{`\u2630`}
+			</motion.div>
+		</div>
 	);
 }
 
